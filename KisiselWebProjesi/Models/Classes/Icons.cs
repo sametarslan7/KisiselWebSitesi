0ccd7c1e71ef2bdd00fa72c6265778947a5e8058
Samet Arslan <sametarslan1129@gmail.com>
2024-09-02 06:08:20 +0300

    mvc e giris yapıldı
    vaşlangıç için controller view ve model eklendi

---
+++ KisiselWebProjesi/Models/Classes/Icons.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace KisiselWebProjesi.Models.Classes
+{
+    public class Icons
+    {
+        [Key]
+        public int id { get; set; }
+        public string icon_name  { get; set; }
+        public string icon_link { get; set; }
+    }
+}
---
+++ KisiselWebProjesi/Models/Classes/MainPage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace KisiselWebProjesi.Models.Classes
+{
+    public class MainPage
+    {
+        [Key]
+        public int id { get; set; }
+        public string profile_photo { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public string contact { get; set; }
+    }
+}
