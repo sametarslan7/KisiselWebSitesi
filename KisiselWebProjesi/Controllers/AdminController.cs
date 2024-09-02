86162230f0425d7357d6995193b4b5efcbeba544
Samet Arslan <sametarslan1129@gmail.com>
2024-09-02 09:55:32 +0300

    admin sayfasında veri çekme işlemi yapıldı
    admin sayfasında veri çekme işlemi yapıldı

---
+++ KisiselWebProjesi/Controllers/AdminController.cs
@@ -0,0 +1,22 @@
+﻿using KisiselWebProjesi.Models.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KisiselWebProjesi.Controllers
+{
+    public class AdminController : Controller
+    {
+        Context c = new Context();
+
+        // GET: Admin
+        public ActionResult Index()
+        {
+            var deger=c.MainPages.ToList();
+
+            return View(deger);
+        }
+    }
+}
